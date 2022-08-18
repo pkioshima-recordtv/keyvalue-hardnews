5964d8c18859bf74c29af1677989ed87f925772d
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-08-18 15:54:27 -0300

    Update ros_validation.js

--- ros_validation.js
+++ ros_validation.js
@@ -11,3 +11,18 @@ var bodytext = bodytext.replace(/[íì]/g,"i");
 var bodytext = bodytext.replace(/[õôö]/g,"o");
 var bodytext = bodytext.replace(/[úùü]/g,"u");
 var bodytext = bodytext.replace(/[ç]/g,"c");
+
+if (document.querySelector('meta[property="og:type"]').content != "article") {
+    var hardnews = "0";
+    }else{
+    	if(words.test(bodytext)){
+    			var hardnews = "1"
+    		}else{
+    			var hardnews = "0"
+    		}    		
+    }  
+  	  (function() {
+	    var s = document.createElement('script');
+	    s.innerHTML = 'googletag.cmd.push(function() { googletag.pubads().setTargeting("hardnews",hardnews); });';
+	    document.head.appendChild(s);
+	  })();
